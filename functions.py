7c857cbbf7769713bea0c55c34a7554832587c50
Maciej Dems <maciej.dems@p.lodz.pl>
2020-10-26 11:57:01 +0100

    Variables samples

---
+++ functions.py
@@ -0,0 +1,24 @@
+
+def sum(item1=0, item2=0, item3=0):
+    result = item1 + item2 + item3
+    return result
+
+total = sum(1, 2, 3)
+print(total)
+
+# total = sum(0.99, 3.98)
+# print(f"We payed in total {total} PLN")
+
+# # %%
+# def format(arg1, arg2=None, arg3=None):
+#     if arg2 is None:
+#         arg2 = 'missing'
+#     if arg3 is None:
+#         arg3 = "???"
+#     oh_shit
+#     return f"arg1: {arg1}, arg2: {arg2}, arg3: {arg3}"
+
+# # %%
+# print(format('one', 'two'))
+
+# # %%
---
+++ variable.py
@@ -0,0 +1,7 @@
+capitals = {       # after opening the parenthesis, you can go to a new line
+     'Poland': 'Warsaw',
+     'Germany': 'Berlin',
+     'USA': 'Washington'
+}
+
+print(capitals)
